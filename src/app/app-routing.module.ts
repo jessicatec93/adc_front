c3c0aba327ab5ff51e08d13d2a79acc1481c3114
Lechuga <a01793512@tec.mx>
2023-11-08 01:16:23 -0600

    ADDED: component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductListComponent } from './product/product-list/product-list.component';
+import { OrderListComponent } from './order/order-list/order-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/', pathMatch: 'full' },
+  {path:'/product-list', component: ProductListComponent},
+  {path:'/order-list', component: OrderListComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
