a211b869c6bff868cddc2107aec2d9a323b0f16c
Lechuga <a01793512@tec.mx>
2023-11-11 17:44:32 -0600

    UPDATED: lib boost

--- src/app/product/product-list/product-list.component.ts
+++ src/app/product/product-list/product-list.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { ProductService } from './product.service';
-import { Observable } from 'rxjs';
 import { Product } from './schemas/product';
 import { Pagination } from './schemas/Pagination';
 
